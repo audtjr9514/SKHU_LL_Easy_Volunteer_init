96655474aa967df0d4f3ab54f17cc70d7c16d29f
myeongs14 <audtjr9514@gmail.com>
2019-07-16 03:04:15 +0900

    init

--- easy_volunteer/easyvolunteer/models.py
+++ easy_volunteer/easyvolunteer/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class GeneralUser(AbstractUser):
+    codeNum = models.IntegerField()
+    phoneNum = models.IntegerField()
+    job = models.ForeignKey('Job', on_delete=models.CASCADE)
+
+    def __str(self):
+        return self.first_name
+
+class Job(models.Model):
+    job = models.CharField(max_length=20)
+    def __str__(self):
+        return self.Name
+
+
+class Product(models.Model):
+    Namn = models.CharField(max_length=30)
+    brand = models.ForeignKey('Brand', on_delete=models.CASCADE)
+    point = models.IntegerField()
+    def __str__(self):
+        return self.Name
+
+class Brand(models.Model):
+    name = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='images/', blank=True)
+    def __str__(self):
+        return self.Name
 
 # Create your models here.
--- easy_volunteer/easyvolunteer/views.py
+++ easy_volunteer/easyvolunteer/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+from django.http import HttpResponse
 
 # 맨 첫화면 _ 메인 페이지
 def main(request):
@@ -10,19 +14,31 @@ def introduce(request):
 
 # 회원가입에서 기관/일반회원을 고를수 있는 선택 페이지
 def select(request):
-    return render(request, 'selct.html')
+    return render(request, 'select.html')
 
-# 회원가입 페이지
-def signup(request):
-    return render(request, 'signup.html')
+# 기관 회원가입 페이지
+def organ_signup(request):
+    return render(request, 'organ_signup.html')
+
+# 일반 회원가입 페이지
+def user_signup(request):
+    return render(request, 'user_signup.html')
 
 # 로그인 페이지
 def login(request):
     return render(request, 'login.html')
 
-# 마이페이지
+# 일반 회원 마이페이지
 def mypage(request):
     return render(request, 'mypage.html')
+
+# 기관용 마이페이지
+def organ_mypage(request):
+    return render(request, 'organ_mypage.html')
+
+# 기관에서 봉사 등록 페이지
+def register(request):
+    return render(request, 'register.html')
 
 # 봉사활동을 선택할 수 있는 페이지
 def quest(request):
@@ -31,6 +47,3 @@ def quest(request):
 # 회원이 포인트를 사용할 수 있는 페이지
 def point(request):
     return render(request, 'point.html')
-
-    
-# Create your views here.
